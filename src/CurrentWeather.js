b3c777d7e7ae2c675521c075669bf06501afe106
TetianaIvashyna <ivashyna.t@gmail.com>
2023-04-22 15:14:51 +0200

    A component "WeatherExtra" is removed

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Temperature from "./Temperature";
 import WeatherPicture from "./WeatherPicture";
-import WeatherExtras from "./WeatherExtras";
+import Wind from "./Wind";
+import Visibility from "./Visibility";
+import WindDirection from "./WindDirection";
+import Humidity from "./Humidity";
 
 export default function CurrentWeather() {
   return (
@@ -9,13 +12,23 @@ export default function CurrentWeather() {
       <div className="container">
         <div className="row">
           <div className="col-sm-4">
-            <Temperature value={18} />
+            <Temperature value={15} />
           </div>
           <div className="col-sm-4">
             <WeatherPicture icon="☀️" />
           </div>
           <div className="col-sm-4">
-            <WeatherExtras />
+            <div className="WeatherExtras text-center">
+        <div className="ImportantExtras">
+            <Wind speed={3} />
+        </div>
+      
+      <div className="extras mt-4 d-none d-sm-block">
+        <Visibility value={10000} />
+        <WindDirection value="North" />
+        <Humidity value={48} />
+      </div>
+    </div>
           </div>
         </div>
       </div>
