e46292e740a2369afb8f845a6e783f2559a45413
TetianaIvashyna <ivashyna.t@gmail.com>
2023-04-27 18:51:26 +0200

    The possibility to choose Celsius or Fahrenheit is added.

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Temperature(props) {
+  let [temperature, setTemperature] = useState(Math.round(props.value));
+  let [isCelsius, setIsCelsius] = useState(true);
+
+  function toCelsius() {
+    setTemperature(Math.round(props.value));
+    setIsCelsius(true);
+  }
+
+  function toFahrenheit() {
+    let fahrenheit = props.value * (9 / 5) + 32;
+    setTemperature(Math.round(fahrenheit));
+    setIsCelsius(false);
+  }
+
 
   return (
   <div className="Temperature text-center">
-    <h2>{props.value}&deg; </h2>
-    <p><a href="/">C</a> | <a href="/">F</a></p>
+    <h2>{temperature}&deg; </h2>
+    { (isCelsius) ? <span className="big href-role m-2" onClick={toCelsius}>C</span> : <span className="href-role m-2" onClick={toCelsius}>C</span>} |  
+    { (isCelsius) ? <span className="href-role m-2" onClick={toFahrenheit}>F</span> : <span className="big href-role m-2" onClick={toFahrenheit}>F</span>}
     </div>
     );
 }
