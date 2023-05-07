147aaff1d32a54803cf91a78aaa0a6e2fcc1899d
TetianaIvashyna <ivashyna.t@gmail.com>
2023-05-07 12:30:38 +0200

    Cmall Changes

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -76,7 +76,7 @@ export default function Hamburger() {
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto ms-3 mb-2 mb-lg-0 text-start">
+                            <ul className="navbar-nav me-auto ms-3 mb-2 mb-lg-0 text-end">
                                 <li className="nav-item dropdown">
                                     <span className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="true">
                                     My Cities
