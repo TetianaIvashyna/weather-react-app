704100dd0e952217976320d2233d0c92d0953e56
TetianaIvashyna <ivashyna.t@gmail.com>
2023-06-13 18:12:57 +0200

    all icons are hidden

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import Hamburger from "./Hamburger";
 import Footer from "./Footer";
 import "./Styles.css"
-import AllIcons from "./AllIcons";
+// import AllIcons from "./AllIcons";
 
 function App() {
   return (
     <div className="App">
       <div className="app-container container text-center">
         <Hamburger />
-        <AllIcons size={36}/>
+        
         <Footer />
         
         
