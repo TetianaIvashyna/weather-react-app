ed2a8e00fc51de28057d807028e0f98937521065
TetianaIvashyna <ivashyna.t@gmail.com>
2023-06-13 17:37:36 +0200

    small corections to see beret the colours of icons

--- src/AllIcons.js
+++ src/AllIcons.js
@@ -45,7 +45,7 @@ export default function AllIcons(props) {
   <div className="container">
     <div className="row">
         {iconsArray.map(function (item, index) {
-            return(
+            if (index%2 === 0) return(
             <div className="col border rounded" key={index}>
                 <div className="text-center">{item.desc}</div>
                     <ReactAnimatedWeather
@@ -56,6 +56,7 @@ export default function AllIcons(props) {
                     /> 
                 <div className="text-center">{item.color}</div>
             </div>);
+            else return null;
         })};
     </div>
   </div>);
